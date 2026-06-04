505c0986a740c24129f449a1986764255e81ae44
Avinash Aade <avinash.aade.14@gmail.com>
2026-06-04 13:03:02 +0530

    Headless-fix

--- src/test/java/utils/DriverFactory.java
+++ src/test/java/utils/DriverFactory.java
@@ -3,6 +3,7 @@ package utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
@@ -14,7 +15,14 @@ public class DriverFactory {
 
             WebDriverManager.chromedriver().setup();
 
-            driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+
+            driver = new ChromeDriver(options);
 
         } else {
 
@@ -28,14 +36,12 @@ public class DriverFactory {
     }
 
     public static WebDriver getDriver() {
-
         return driver;
     }
 
     public static void quitDriver() {
 
         if (driver != null) {
-
             driver.quit();
         }
     }
